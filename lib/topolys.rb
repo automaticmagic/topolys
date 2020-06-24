b27474a0127d766b18bae85295c81dd0e2bf9573
rd2Bourgeois <{ID}+{username}@users.noreply.github.com>
2020-06-24 08:09:55 -0400

    Added basic P3D & V3D class methods and interactions

--- lib/topolys.rb
+++ lib/topolys.rb
@@ -30,26 +30,20 @@ module Topolys
     end
   end
 
-  #p D3.new(1, 2, 3) == D3.new(1, 2, 3)    # => true
-  #p D3.new(1, 2, 3) == D3.new(1, 2, 4)    # => false
-
   class P3D < D3 # 3D point
 
     # @return [Hash] linked E3D objects
     attr_reader :edges
 
     ##
-    # Initialize a V3D object
+    # Initialize a P3D object
     #
     # @param [Float] X-coordinate
     # @param [Float] Y-coordinate
     # @param [Float] Z-coordinate
-    def initialize(x, y, z)
-      #@id = SecureRandom.uuid
-      #@x = x
-      #@y = y
-      #@z = z
-      @edges = {} # @edges[id:] = < E3D object >
+    def initialize(x:0, y:0, z:0)
+      super
+      @edges = {}
     end
 
     ##
@@ -57,8 +51,9 @@ module Topolys
     #
     # @param [E3D] edge An edge to link
     def link(edge)
-      @edges[edge.id] = edge if edge && edge.is_a?(TOPOLYS::E3D)
-      #@edges.uniq! { |id, e| e[:id] } # TODO ... shouldn't be necessary with unique hash keys
+      if edge && edge.is_a?(Topolys::E3D)
+        @edges[edge] = edge.object_id unless @edges.key?(edge)
+      end
     end
 
     ##
@@ -66,18 +61,59 @@ module Topolys
     #
     # @param [E3D] edge An edge to unlink
     def unlink(edge)
-      @edges.reject!{ |id,e| e[id] == edge.id } # check for has_key? instead
+      @edges.reject!{ |e, id| e == edge } # define
     end
 
     ##
-    # Generates a 3D vector, from self to input vertex
+    # Generates a V3D vector, by subtracting self from 'other'
     #
-    # @param [V3D] vertex A 3D vertex
+    # @param [P3D] other A P3D point
     #
-          # @return [Hash] Returns 3D hash of floats for vector x, y, z dimensions
-        #  def vector(vertex1, vertex2)
-        #    return [vertex2.x-vertex1.x, vertex2.y-vertex1.y, vertex2.z-vertex1.z]
-        #  end
-
+    # @return [V3D] Returns a new V3D vector, 'nil' if 'other' not a P3D point
+    def -(other)
+      if other && other.is_a?(Topolys::P3D)
+        return Topolys::V3D.new(x:(other.x - @x),
+                                y:(other.y - @y),
+                                z:(other.z - @z))
+      end
+      return nil
+    end
   end # P3D
+
+  class V3D < D3 # 3D vector
+    ##
+    # Initialize a V3D object
+    #
+    # @param [Float] X-coordinate
+    # @param [Float] Y-coordinate
+    # @param [Float] Z-coordinate
+    def initialize(x:0, y:0, z:0)
+      super
+    end
+
+    # Get vector magnitude (or length)
+    #
+    # @return [Float] Returns magnitude of the 3D vector
+    def magnitude
+      Math.sqrt(x**2 + y**2 + z**2)
+    end
+
+    # this is an example of vector operations in Ruby::Vector
+    # def normalize
+    #  n = magnitude
+    #  raise ZeroVectorError, "Zero vectors can not be normalized" if n == 0
+    #  self / n
+    # end
+
+    # or another useful one ...
+    # File lib/matrix.rb, line 2085
+    # def angle_with(v)
+      # raise TypeError, "Expected a Vector, got a #{v.class}" unless v.is_a?(Vector)
+      # Vector.Raise ErrDimensionMismatch if size != v.size
+      # prod = magnitude * v.magnitude
+      # raise ZeroVectorError, "Can't get angle of zero vector" if prod == 0
+      # Math.acos( inner_product(v) / prod )
+    # end
+
+  end # V3D
 end # TOPOLYS
--- spec/topolys_spec.rb
+++ spec/topolys_spec.rb
@@ -9,26 +9,41 @@ RSpec.describe Topolys do
   it "checks D3 class functionality" do
     d3_1 = Topolys::D3.new(x:1, y:1, z:1)
     expect(d3_1.is_a?(Topolys::D3)).to eq(true)
-    d3_2 = Topolys::D3.new(y:1, x:0) # 
+    d3_2 = Topolys::D3.new(y:1, x:0)    # dimensions or order: optional
     expect(d3_2.is_a?(Topolys::D3)).to eq(true)
     expect(d3_1 == d3_2).to eq(false)
     expect(d3_1.eql?(d3_2)).to eq(false)
     expect(d3_2.z).to eq(0)
     d3_2.x = d3_2.z = 1
-    expect(d3_1 == d3_2).to eq(true)
-    expect(d3_1.eql?(d3_2)).to eq(true)
+    expect(d3_1 == d3_2).to eq(true)    # overridden '==' & 'eql?', so
+    expect(d3_1.eql?(d3_2)).to eq(true) # equality based on coordinates/class
+
     d3_3 = Topolys::D3.new
     d3_4 = d3_3.clone
     expect(d3_3).to eq(d3_4)
-    expect(d3_1.to_s).not_to eq(d3_2.to_s)
+
+    # ... but not 'equal?', so can always rely on uniqueness of objects
+    expect(d3_3.equal?(d3_4)).to eq(false)
+    # ... variants
+    expect(d3_3.to_s).not_to eq(d3_4.to_s)
     expect(d3_3.object_id).not_to eq(d3_4.object_id)
 
-    d3_array = [d3_1, d3_2, d3_3, d3_4]
+    # if instead d3_4 is assigned d3_3 ...
+    d3_4 = d3_3
+    expect(d3_3).to eq(d3_4)
+    expect(d3_3.equal?(d3_4)).to eq(true)
+    expect(d3_3.to_s).to eq(d3_4.to_s)
+    expect(d3_3.object_id).to eq(d3_4.object_id)
+
+    # reverting back to a new object
+    d3_4 = d3_3.clone
+
+    d3_array = [d3_1, d3_2, d3_3, d3_4] # demo: array of D3 objects
     expect(d3_array.size).to eq(4)
-    d3_u_array = d3_array.uniq # ensuring D3 object uniqueness
+    d3_u_array = d3_array.uniq          # uniqueness based on coordinates/class
     expect(d3_u_array.size).to eq(2)
 
-    d3_hash = {} # demo: D3 objects as unique hash keys
+    d3_hash = {}                        # demo: D3 objects as unique hash keys
     d3_hash[d3_1] = d3_1.object_id
     d3_hash[d3_2] = d3_2.object_id
     d3_hash[d3_3] = d3_3.object_id
@@ -43,7 +58,7 @@ RSpec.describe Topolys do
     expect(d3_hash.value?(d3_3.object_id)).to eq(false)
     expect(d3_hash.value?(d3_4.object_id)).to eq(true)
 
-    d3_hash2 = {} # demo: D3 as unique hash keys
+    d3_hash2 = {}                       # demo: D3 as unique hash keys
     d3_hash2[d3_1] = d3_1.object_id unless d3_hash2.key?(d3_1)
     d3_hash2[d3_2] = d3_2.object_id unless d3_hash2.key?(d3_2)
     d3_hash2[d3_3] = d3_3.object_id unless d3_hash2.key?(d3_3)
@@ -52,6 +67,27 @@ RSpec.describe Topolys do
     expect(d3_hash2.value?(d3_2.object_id)).to eq(false)
     expect(d3_hash2.value?(d3_3.object_id)).to eq(true)
     expect(d3_hash2.value?(d3_4.object_id)).to eq(false)
+  end # D3
 
-  end
-end
+  it "checks basic P3D & V3D class functionality" do
+    p3D_1 = Topolys::P3D.new(x:0, y:0, z:0)
+    p3D_2 = Topolys::P3D.new(x:0, y:0, z:0)
+
+    # inherits D3 ... '==', 'eql?' & 'equal?' overridden methods
+    expect(p3D_1.is_a?(Topolys::P3D)).to eq(true)
+    expect(p3D_1).to eq(p3D_2)
+    expect(p3D_1.eql?(p3D_2)).to eq(true)
+    expect(p3D_1 == p3D_2).to eq(true)
+    expect(p3D_1.equal?(p3D_2)).to eq(false)
+
+    p3D_2.x = p3D_2.y = p3D_2.z = 2
+
+    v3D_1 = p3D_1 - p3D_2                 # basic point & vector operation
+    unless v3D_1.nil?
+      expect(p3D_2 == v3D_1).to eq(false)   # despite having same XYZ coordinates
+      expect(p3D_2.eql?(v3D_1)).to eq(false)
+      expect(v3D_1.magnitude).to be_within(0.0001).of(3.4641016151377544)
+    end
+
+  end # P3D & V3D
+end # Topolys
